de210c73d07f3442d00b6c795d27b5310cbde4a5
vasavivodnala <vasavivodnala.v@gmail.com>
2024-12-30 18:49:35 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-docker-compose down
-docker-compose up -d
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u vasavivodnala -p Asviarha@2014
+    docker tag test vasavivodnala/dev:latest
+    docker push vasavivodnala/dev:latest
+
+elif [[ $GIT_BRANCH == "origin/master" ]]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u vasavivodnala -p Asviarha@2014
+    docker tag test vasavivodnala/prod:latest 
+    docker push vasavivodnala/prod:latest
+fi
+
 
