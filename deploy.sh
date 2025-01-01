501991aa1ea037a002c519ca6fc6dd33c79b03f9
vasavivodnala <vasavivodnala.v@gmail.com>
2025-01-01 11:02:52 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Deploy Docker container
-docker run -d -p 80:80 --name app vasavivodnala/apping:latest
+docker run -d -p 80:80 --name apping vasavivodnala/apping:latest
 echo "Application deployed successfully."
 
 
