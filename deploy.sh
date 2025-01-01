daec1070894f937314b0f45f6c47562e3a62f24c
vasavivodnala <vasavivodnala.v@gmail.com>
2025-01-01 21:47:20 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-# Deploy Docker container
-docker run -d -p 80:80 --name apping vasavivodnala/apping:latest
-echo "Application deployed successfully."
+if [[ $GIT_BRANCH == "origin/dev" ]]; then
+    # Build your project
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u vasavivodnala -p Asviarha@2014
+    docker tag latest vasavivodnala/dev
+    docker push vasavivodnala/dev
 
-
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
+    sh 'chmod +x build.sh'
+    sh './build.sh'
+    docker login -u vasavivodnala -p Asviarha@2014
+    docker tag latest vasavivodnala/prod
+    docker push vasavivodnala/prod
+fi
