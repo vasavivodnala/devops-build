78d02bd1c0f6ff5fb1eb8e9031ea4f22dd73394e
vasavivodnala <vasavivodnala.v@gmail.com>
2025-01-28 20:35:10 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+echo "Application deployed successfully on port 80"
 if [[ $GIT_BRANCH == "origin/dev" ]]; then
     # Build your project
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u vasavivodnala -p Asviarha@2014
-    docker tag appq vasavivodnala/dev
-    docker push vasavivodnala/dev
+    docker tag project vasavivodnala/dev:latest
+    docker push vasavivodnala/dev:latest
 
-elif [[ $GIT_BRANCH == "origin/master" ]]; then
+elif [[ $GIT_BRANCH == "origin/main" ]]; then
     sh 'chmod +x build.sh'
     sh './build.sh'
     docker login -u vasavivodnala -p Asviarha@2014
-    docker tag appq vasavivodnala/prod
-    docker push vasavivodnala/prod
+    docker tag project vasavivodnala/prod:latest
+    docker push vasavivodnala/prod:latest
 fi
+~
